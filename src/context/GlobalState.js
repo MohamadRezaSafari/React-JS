5b0ceac0db110129c69658cd4a4639b27378a688
Reza <Reza@DESKTOP-IHDE003>
2023-08-21 13:19:07 +0330

    Finish

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -10,14 +10,29 @@ const initialState = {
     ]
 }
 
-
 export const GlobalContext = createContext(initialState);
 
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
 
+    function deleteTransaction(id){
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        })
+    }
+
+    function addTransaction(transaction){
+        dispatch({
+            type: 'Add_TRANSACTION',
+            payload: transaction
+        })
+    }
+
     return (<GlobalContext.Provider value={{
-        transactions: state.transactions
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
     }}>
         {children}
     </GlobalContext.Provider>);
